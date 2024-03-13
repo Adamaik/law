9c350f5a08c9edd5d7b62f3ebd633e3c159e0067
Your Name <you@example.com>
2024-03-13 21:41:26 +0800

    first commit

---
+++ src/main/java/com/adamaik/law/LawApplication.java
@@ -0,0 +1,16 @@
+package com.adamaik.law;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Adamaik
+ */
+@SpringBootApplication
+public class LawApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LawApplication.class, args);
+    }
+
+}
